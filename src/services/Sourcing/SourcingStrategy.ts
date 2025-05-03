21137bbac294abfbddf54f955c6a2d5466fb1e27
Ambrose821 <mclaughlinambrose@gmail.com>
2025-05-02 23:37:23 -0400

    fix sonarqube

--- src/services/Sourcing/SourcingStrategy.ts
+++ src/services/Sourcing/SourcingStrategy.ts
@@ -1,21 +1,21 @@
-interface SourcingStrategy {
+interface RssSourcingStrategy {
     sourceFeed(feed:any):Object |null;
 }
 
-class nineStrategy implements SourcingStrategy{
+class nineStrategy implements RssSourcingStrategy{
     sourceFeed(feed:any):Object |null{
         return null
     }
 }
 
-class newsIOstrategy implements SourcingStrategy{
+class newsIOstrategy implements RssSourcingStrategy{
     sourceFeed(feed:any):Object |null{
         return null
     }
 
 }
 
-class rssAppStrategy implements SourcingStrategy{
+class rssAppStrategy implements RssSourcingStrategy{
     sourceFeed(feed:any):Object |null{
         return null
     }
